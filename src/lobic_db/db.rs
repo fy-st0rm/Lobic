2c845f72340f5a8d174c5021459655034e81f30f
Rasaili-rain <saditrasaili063@gmail.com>
2024-11-26 08:13:42 -0800

    made migrations use relative paths

--- src/lobic_db/db.rs
+++ src/lobic_db/db.rs
@@ -1,21 +1,18 @@
-use diesel::r2d2::{
-	ConnectionManager,
-	Pool,
-};
 use diesel::prelude::*;
+use diesel::r2d2::{ConnectionManager, Pool};
 use dotenv::dotenv;
 
 pub type DatabasePool = Pool<ConnectionManager<SqliteConnection>>;
 
 pub fn generate_db_pool() -> DatabasePool {
-	dotenv().ok();
+    dotenv().ok();
 
-	let database_url = std::env::var("DATABASE_URL")
-		.expect("DATABASE_URL must be set in .env file");
+    let database_url =
+        std::env::var("DATABASE_URL").expect("DATABASE_URL must be set in .env file");
 
-	let manager = ConnectionManager::<SqliteConnection>::new(database_url);
-	Pool::builder()
-		.max_size(5)
-		.build(manager)
-		.expect("Failed to create pool")
+    let manager = ConnectionManager::<SqliteConnection>::new(database_url);
+    Pool::builder()
+        .max_size(5)
+        .build(manager)
+        .expect("Failed to create pool")
 }
