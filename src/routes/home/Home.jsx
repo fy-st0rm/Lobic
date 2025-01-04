27871a2624ef87026734bdf31ada259bb557165f
optus-56 <bijanbakabal45@gmail.com>
2025-01-04 11:07:09 +0545

    added  scrollable page in home

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -2,14 +2,26 @@ import { useNavigate } from "react-router-dom";
 
 import MusicPlayer from "../../components/MusicPlayer/MusicPlayer";
 import NavBar from "../../components/NavBar/NavBar.jsx"
+import MusicList from "../../components/MusicList/MusicList.jsx"
+import './Home.css'
 
 function Home() {
 
 	return (
 		<>
+		<div className="home-container">
 			<NavBar />
+			<div className="scrollable-area">
 			<MusicList />
+			<MusicList />
+			<MusicList />
+			<MusicList />
+			</div>
+
+
 			<MusicPlayer />
+		</div>
+			
 		</>
 	);
 
