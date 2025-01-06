7675102179d6d13ea995f3cc5e02aacad62eb962
optus-56 <bijanbakabal45@gmail.com>
2025-01-06 23:59:06 +0545

    fixed some issues in the home screen

--- src/routes/profile/Profile.jsx
+++ src/routes/profile/Profile.jsx
@@ -1,13 +1,11 @@
 import React, {useState} from 'react'
 import './Profile.css'
 import ProfileCard from '../../components/ProfileCard/ProfileCard';
-import { useAppState } from "../../AppState.jsx";
-import { SERVER_IP, WS_SERVER_IP, wsSend, OpCode } from "../../const.jsx";
 
 function Profile() {
     return (
         <>
-            <ProfileCard />
+    
         </>
     );
 }
