f59115e01177056167c8fa6532bbda9db6d3d0d1
Samki <ssameep5@gmail.com>
2025-01-08 04:29:48 +0000

    music playable from the playlist page

--- src/components/SongContainer/SongContainer.jsx
+++ src/components/SongContainer/SongContainer.jsx
@@ -1,22 +1,46 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import SongInfo from "../SongInfo/SongInfo";
-import{SERVER_IP} from "../../const.jsx";
+import { SERVER_IP, MPState } from "../../const.jsx";
+import { useAppState } from "../../AppState.jsx";
 function SongContainer() {
+  const { updateMusicData } = useAppState();
   const [musicItems, setMusicItems] = useState([]);
   useEffect(() => {
-      fetchMusicData();
-    }, []);
+    fetchMusicData();
+  }, []);
   const fetchMusicData = async () => {
-      try {
-        const response = await fetch(`${SERVER_IP}/get_music`);
-        if (!response.ok) throw new Error('Failed to fetch music data');
-        const data = await response.json();
-        setMusicItems(data);
-      } catch (err) {
-        setError(err.message);
-      }
-  
-    };
+    try {
+      const response = await fetch(`${SERVER_IP}/get_music`);
+      if (!response.ok) throw new Error('Failed to fetch music data');
+      const data = await response.json();
+      setMusicItems(data);
+
+    } catch (err) {
+      setError(err.message);
+
+    }
+
+
+  };
+  const handleMusicClick = async (item) => {
+    try {
+
+      const coverArt = `${SERVER_IP}/image/${item.id}.png`;
+
+      // Updating Music State globally
+      updateMusicData(
+        item.id,
+        item.title,
+        item.artist,
+        coverArt,
+        MPState.CHANGE
+      );
+    } catch (err) {
+      console.error('Failed to fetch music URL:', err);
+
+    } finally {
+    }
+  };
   return (
     <div className="absolute top-[11%] right-[4%] bg-primary-100 opacity-65 rounded-[18px] h-[75%] w-[40%] min-w-[300px] flex flex-col pb-5">
       <div className="sticky top-0 bg-primary-100 rounded-[18px]  z-10">
@@ -27,17 +51,20 @@ function SongContainer() {
         </div>
         <div className="mx-5 left-1 h-[2px] bg-white opacity-50 rounded-[10px] my-1"></div>
       </div>
-      
-      
+
+
       <div className="overflow-y-auto flex-1">
         {musicItems.map((item) => (
-          <SongInfo
-            songName={item.title}
-            artistName={item.artist}
-            duration={item.album}
-            addedBy={item.addedBy}
-            coverImg={`${SERVER_IP}/image/${item.id}.png`}
-          />
+          <div onClick={() => handleMusicClick(item)}>
+            <SongInfo
+              songName={item.title}
+              artistName={item.artist}
+              duration={item.album}
+              addedBy={item.addedBy}
+              coverImg={`${SERVER_IP}/image/${item.id}.png`}
+
+            />
+          </div>
         ))}
       </div>
     </div>
