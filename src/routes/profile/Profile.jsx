eb7a9849722a7a2cd86871fe30908386b4651b42
optus-56 <bijanbakabal45@gmail.com>
2025-01-08 16:06:14 +0545

    added playlist list to the profile page

--- src/routes/profile/Profile.jsx
+++ src/routes/profile/Profile.jsx
@@ -1,16 +1,25 @@
-import React, {useState} from 'react'
-import './Profile.css'
+import React from 'react';
+import './Profile.css';
 import ProfileCard from '../../components/ProfileCard/ProfileCard';
-import SearchList from '../../components/SearchList/SearchList.jsx'
+import SearchList from '../../components/SearchList/SearchList.jsx';
+import PlaylistsContainer from '../../components/PlaylistsContainer/PlaylistsContainer.jsx';
+
 function Profile() {
     return (
         <>
-        <div className='profile-page-container'>\
-            <div className='profile-info'>
-                <ProfileCard usertag={`@bijan`} username={`Bijan Thapa`} friendcount={`0 friends`}/>
-            </div>
-            <div className="search-list-container">
-                <SearchList />
+        <div className="home-container">
+            <div className="profile-page-container">
+                <div className="main-content">
+                    <div className="profile-info">
+                        <ProfileCard usertag={`@bijan`} username={`Bijan Thapa`} friendcount={`0 friends`} />
+                    </div>
+                    <div className="playlists-container">
+                        <PlaylistsContainer />
+                    </div>
+                </div>
+                <div className="search-list-container">
+                    <SearchList />
+                </div>
             </div>
         </div>
         </>
