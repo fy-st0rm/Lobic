56325ab6ca854fbd3fda3dec331820b4ca104a3f
Rasaili-rain <saditrasaili063@gmail.com>
2025-02-09 07:29:01 -0800

    maybe?

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -4,24 +4,24 @@ import { MusicListsProvider } from "@/providers/MusicListContextProvider";
 import {useSidebarState} from "@/components/SideBar/SideBar";
 
 function Home() {
-  const { isExtended } = useSidebarState();
+	const { isExtended } = useSidebarState();
 
-  return (
+	return (
 	// @TODO: remove the top-[115px] 
-	  <MusicListsProvider> 
-      <div className={`h-[80vh] overflow-scroll relative top-[115px] transition-all ${
-        isExtended ? 'w-[86vw] left-[13vw]' : 'w-[93vw] left-[6vw]'
-      }`}>
-        <div className="scrollable-area">
-          <MusicList list_title="Liked Songs" />
-          <MusicList list_title="Featured Music" />
-          <MusicList list_title="Recently Played" />
-          <MusicList list_title="Trending Now" />
-          <MusicList list_title="My Top Tracks" />
-        </div>
-      </div>
-    </MusicListsProvider>
-  );
+		<MusicListsProvider> 
+			<div className={`flex flex-col h-[80vh] overflow-hidden relative top-[115px] transition-all ${
+				isExtended ? 'w-[86vw] left-[13vw]' : 'w-[93vw] left-[6vw]'
+			}`}>
+				<div className="scrollable-area">
+					<MusicList list_title="Liked Songs" />
+					<MusicList list_title="Featured Music" />
+					<MusicList list_title="Recently Played" />
+					<MusicList list_title="Trending Now" />
+					<MusicList list_title="My Top Tracks" />
+				</div>
+			</div>
+		</MusicListsProvider>
+	);
 }
 
 export default React.memo(Home);
